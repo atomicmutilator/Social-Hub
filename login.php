4ca6b19e8c944e371e454a1452adf0ec33f9ffef
atomicmutilator <robert.sauer1337@gmail.com>
2017-10-19 07:26:22 +0000

    Files reorganized. Added container files.

--- login.php
+++
@@ -1,7 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="post" action="login_action.php">
-Email: <input type="text" name="email" /> <br>
-Password: <input type="password" name="password1" /> <br>
-<input type="submit" name="button" value="Login" />&nbsp;&nbsp;
-<a href="register.php">Register</a>
-</form>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();
-// delete all session
-session_destroy(); 
-echo 'Logout......';
-echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
-?>
